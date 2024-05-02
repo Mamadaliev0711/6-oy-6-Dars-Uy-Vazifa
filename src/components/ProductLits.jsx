14fc0d5302c546a7b89f6fbe8c8267de3a6c9054
Mamadaliev0711 <mamadaliyevsunnatullo@gmail.com>
2024-05-02 15:51:05 +0500

    vazifa bajarildi

---
+++ src/components/ProductLits.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ProductList({ products }) {
+  return (
+    <ul className="  grid  md:grid-cols-2 lg:grid-cols-3  gap-3 ">
+      {products &&
+        products.products.map((product) => (
+          <li
+            key={product.id}
+            className="card card-compact  bg-base-100 shadow-xl"
+          >
+            <figure className="border-b-2   border-orange-300">
+              <img
+                src={product.thumbnail}
+                alt={product.name}
+                className="w-full  h-52 object-cover"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{product.title}</h2>
+              <p className="lign-clamp-3">{product.description}</p>
+              <div className="card-actions justify-bet  items-center">
+                <p className="text-2xl flex gap-1 items-center ">
+                  $ {product.price}
+                </p>
+                <Link
+                  to={`/product/${product.id}`}
+                  className="btn
+ btn-primary"
+                >
+                  <i class="bi bi-tag"></i>
+                  Buy Now
+                </Link>
+              </div>
+            </div>
+          </li>
+        ))}
+    </ul>
+  );
+}
+
+export default ProductList;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,16 @@
+export default function Contact() {
+  return (
+    <div className="align-content">
+      <h1>Contact</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+        blanditiis omnis unde nam soluta nihil numquam ex quod molestiae
+        explicabo. Voluptas magni suscipit in eveniet esse hic quasi repudiandae
+        cum laborum est amet minima ipsam eius, nisi accusantium itaque veniam
+        omnis, ad quaerat quos, eos alias quae odit. Maiores, voluptas odit amet
+        natus dolore reiciendis facere iure itaque tenetur nemo dolorum aperiam
+        necessitatibus! Tempora, repudiandae? Minima eligendi quas nisi vero.
+      </p>
+    </div>
+  );
+}
